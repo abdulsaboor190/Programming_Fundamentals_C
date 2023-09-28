7d568f8fd1f4150a2d750f7859b03f747b5b8bf5
Abdul Saboor <140578710+abdulsaboor190@users.noreply.github.com>
2023-09-28 16:57:04 +0500

    Update and rename aaaaa.cpp to Simple Calculator With a single cout.cpp

---
+++ Simple Calculator With a single cout.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+	
+	cout<<" ."<<setw(42)<<setfill('-')<<","<<endl<<"| "<<"."<<setw(39)<<setfill('-')<<","<<" |"<<endl<<"| |  _~_"
+	<<setw(36)<<setfill(' ')<<"| |"<<endl<<"| |   \\/   NUCES INSTRUMENT    T I - 8 6 | |"<<endl
+	<<"| | ."<<setw(35)<<setfill('-')<<","<<" | |"<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | | 1 + 1"<<setw(29)<<setfill(' ')<<"|"<<" | |"
+	<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | |"<<setw(32)<<setfill(' ')<<"2"<<"  | | |"<<endl<<"| | |"<<
+	setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | | 6 * 8"<<setw(29)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | |"<<setw(32)<<setfill(' ')<<"48"<<"  | | |"<<endl<<"| | |"<<
+	setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | | -"<<setw(33)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"
+	<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"
+	<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| | `"<<setw(35)<<setfill('-')<<"'"<<" | |"
+	<<endl<<"| | |"<<setw(35)<<setfill(' ')<<"|"<<" | |"<<endl<<"| "<<"`"<<setw(39)<<setfill('-')<<"'"<<" |"<<endl
+	<<"|     M1      M2     M3     M4     M5      |"<<endl
+	<<"|"<<setw(39)<<setfill(' ')<<".----,  .----, .----, .----,  .----,"<<setw(5)<<setfill(' ')<<"| "<<endl
+	<<"|"<<setw(39)<<setfill(' ')<<"| F1 |  | F2 | | F3 | | F4 |  | F5 |"<<setw(4)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(39)<<setfill(' ')<<"`----'  `----' `----' `----'  `----'"<<setw(4)<<setfill(' ')<<"|"<<endl
+	<<"|-------.________        _________,--------|"<<endl
+	<<"|"<<setw(24)<<setfill(' ')<<" `------'"<<setw(19)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(34)<<setfill(' ')<<"QUIT   MODE        _,-._"<<setw(9)<<setfill(' ')<<"|"<<endl
+	<<"|  .----,  .----, .----,   /\\ \\_^_/  /\\"<<setw(5)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(39)<<setfill(' ')<<"| 2nd|  |Exit| |More|  / /   |    \\ \\"<<setw(4)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(39)<<setfill(' ')<<"`----'  `----' `----'  |<|   0    |>|"<<setw(4)<<setfill(' ')<<"|"<<endl
+	<<"|  alpha   LINK x  INS    \\ \\  _|_   / /"<<setw(4)<<setfill(' ')<<"|"<<endl
+	<<"|  .-----, .-----, .----,  \\/ / v \\  \\/"<<setw(5)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(34)<<setfill(' ')<<"|ALPHA| |x-VAR| | DEL|     -._,-"<<setw(9)<<setfill(' ')<<"|"<<endl
+	<<"|  `-----' `-----' `----'"<<setw(19)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(34)<<setfill(' ')<<"SOLVER  SIMULT  POLY  CATLG-VARS"<<setw(9)<<setfill(' ')<<"|"<<endl
+	<<"|  .-----, .-----, .-----, .-----, .-----, |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"|GRAPH| |TABLE| | PRGH| |CUSTM| |CLEAR| |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"`-----' `-----' `-----' `-----' `-----' |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"10x  A  SIN-1 B  COS-1 C TAN-1 D pi  E  |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<".-----, .-----, .-----, .-----, .-----, |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"| LOG | | SIN | | COS | | TAN | |  ^  | |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"`-----' `-----' `-----' `-----' `-----' |"<<endl
+	<<"|  ex   F  x-1  G  [    H  ]    I  CALC J"<<setw(3)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<".-----, .-----, .-----, .-----, .-----, |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"| LN  | | EE  | |  (  | |  )  | |     | |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"`-----' `-----' `-----' `-----' `-----' |"<<endl
+	<<"|"<<setw(40)<<setfill(' ')<<"./~  K  MATRX L VECTR M CPLX N  MATH 0"<<setw(3)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<".-----, .-----, .-----, .-----, .-----, |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"| X2  | |  7  | |  8  | |  9  | |  x  | |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"`-----' `-----' `-----' `-----' `-----' |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"/_   P  CONS Q  CONV R  STRNG S LIST  T |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<".-----, .-----, .-----, .-----, .-----, |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"|  ,  | |  4  | |  5  | |  6  | |  -  | |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"`-----' `-----' `-----' `-----' `-----' |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"RCL  =  BASE U  TEST V  MEM  W  START X |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<".-----, .-----, .-----, .-----, .-----, |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"| STO>| |  1  | |  2  | |  3  | |  +  | |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"`-----' `-----' `-----' `-----' `-----' |"<<endl
+	<<"|"<<setw(39)<<setfill(' ')<<"OFF    CHAR Y  :    z  ANS  _  ENTRY"<<setw(4)<<setfill(' ')<<"|"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<".-----, .-----, .-----, .-----, .-----, |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"| ON  | |  0  | |  .  | | (-) | |ENTER| |"<<endl
+	<<"|"<<setw(43)<<setfill(' ')<<"`-----' `-----' `-----' `-----' `-----' |"<<endl
+	<<"|  -------"<<setw(34)<<setfill(' ')<<"|"<<endl
+	<<"`\\ "<<setw(41)<<setfill(' ')<<"/'"<<endl
+	<<"  `-."<<setw(37)<<setfill(' ')<<".-'"<<endl
+	<<"     `-"<<setw(33)<<setfill(' ')<<".-'"<<endl
+	<<"       `---_______________________---'"<<endl;	
+
+		
+	
+		return 0;
+}
